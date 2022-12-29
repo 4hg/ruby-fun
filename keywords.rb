1078a5ba190736064da0e58909ebd62caec4969b
South <33138892+4hg@users.noreply.github.com>
2022-12-28 21:16:28 -0600

    add keywords

---
+++ keywords.rb
@@ -0,0 +1,12 @@
+# See https://codegolf.codidact.com/posts/285821/286053 for more info, this producse no output.
+BEGIN{END{alias undef class
+if self&&true||false
+begin rescue
+retry ensure end
+elsif defined?
+for$.in 1do module B
+super until1 end
+redo
+next end
+return def _ =yield1 end
+case __ENCODING__ when __LINE__ then __FILE__ else break while1 until2 unless not nil end}}
