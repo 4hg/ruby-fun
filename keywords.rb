751865bab68cd2e78d43be3057d0d682ea525a63
South <33138892+4hg@users.noreply.github.com>
2022-12-28 21:56:18 -0600

    improvements and fixes to keywords

--- keywords.rb
+++ keywords.rb
@@ -1,12 +1,12 @@
 # See https://codegolf.codidact.com/posts/285821/286053 for more info, this producse no output.
 BEGIN{END{alias undef class
-if self&&true||false
 begin rescue
 retry ensure end
+if self and true or false
 elsif defined?
 for$.in 1do module B
-super until1 end
+super end
 redo
 next end
-return def _ =yield1 end
+return def _ =yield end
 case __ENCODING__ when __LINE__ then __FILE__ else break while1 until2 unless not nil end}}
